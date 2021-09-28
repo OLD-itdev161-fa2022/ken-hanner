97865ef67ae5d6f7f9b0cbc08ceedf991a16b7b4
ken <hannerk@gmatc.matc.edu>
2021-09-28 09:51:14 -0500

    update src/app.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+    <div className="App">
+      <header className="App-header">
+        GoodThings
+      </header>
+    </div>
+  );
+  }
+}
+
+export default App;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         GoodThings
+//       </header>
+//     </div>
+//   );
+// }
+/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn ReactER
+        </a> */
