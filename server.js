4b76246fb2ef8301ed7a97c8327fd16bec7f94d3
ken <hannerk@gmatc.matc.edu>
2021-10-26 14:23:25 -0500

    login user api endpoint

--- server.js
+++ server.js
@@ -80,21 +80,7 @@ app.post(
                 //res.send('User successfully registered');
 
                 //generate and return a JWT token
-                const payload = {
-                    user: {
-                        id: user.id
-                    }
-                };
-
-                jwt.sign(
-                    payload,
-                    config.get('jwtSecret'),
-                    { expiresIn: '10hr' },
-                    (err, token) => {
-                        if (err) throw err;
-                        res.json({ token: token });
-                    }
-                );
+                returnToken(user, res);
             } catch (error) {
                 res.status(500).send('Server error');
             }
@@ -113,6 +99,66 @@ app.get('/api/auth', auth, async (req, res) => {
         res.status(500).send('Unknown server error');
     }
 });
+
+/**
+ * @route POST api/login
+ * @desc Login user
+ */
+app.post(
+    '/api/login',
+    [
+        check('email', 'Please enter a valid email').isEmail(),
+        check('password', 'A password is required').exists()
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        } else {
+            const { email, password } = req.body;
+            try {
+                // check if user exists
+                let user = await User.findOne({ email: email });
+                if (!user) {
+                    return res
+                        .status(400)
+                        .json({ errors: [{ msg: 'Invalid email or password' }] });
+                }
+
+                //check password
+                const match = await bcrypt.compare(password, user.password);
+                if (!match) {
+                    return res
+                        .status(400)
+                        .json({ errors: [{ msg: 'Invalid email or password' }] });
+                }
+
+                //generate and return a JWT token
+                returnToken(user, res);
+            } catch (error) {
+                res.status(500).sent('Server error');
+            }
+        }
+    }
+);
+
+const returnToken = (user, res) => {
+    const payload = {
+        user: {
+            id: user.id
+        }
+    };
+
+    jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: '10hr' },
+        (err, token) => {
+            if (err) throw err;
+            res.json({ token: token });
+        }
+    );
+};
 
 // Connection listener
 const port = 5000;
