f288bef71eb8d21a57f8df0f41f91f6f67877b4a
ken <hannerk@gmatc.matc.edu>
2021-11-16 15:58:36 -0600

    use Post Components in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,13 +4,16 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
+import PostList from './components/PostList/PostList';
+import Post from './components/Post/Post';
 
 class App extends React.Component {
   state = {
     posts: [],
+    post: null,
     token: null,
     user: null
-  }
+  };
 
   componentDidMount() {
       this.authenticateUser();
@@ -78,6 +81,13 @@ class App extends React.Component {
     this.setState({ user: null, token: null });
   }
 
+  viewPost = post => {
+    console.log(`view ${post.title}`);
+    this.setState({
+      post: post
+    });
+  };
+
   render() {
     let { user, posts } = this.state;
     const authProps = {
@@ -110,18 +120,17 @@ class App extends React.Component {
                 {user ? (
                   <React.Fragment>
                     <div>Hello {user}!</div>
-                      <div>
-                        {posts.map(post => (
-                          <div key={posts._id}>
-                            <h1>{posts.title}</h1>
-                            <p>{posts.body}</p>
-                          </div>
-                        ))}
-                      </div>
+                       <PostList
+                        posts={posts}
+                        clickPost={this.viewPost}
+                      />
                 </React.Fragment> 
               ) : ( 
                 <React.Fragment> Please Register or Login </React.Fragment>
               )}
+            </Route>
+            <Route path="/posts/:postId">
+                <Post post={post} />
             </Route>
             <Switch>
               <Route 
